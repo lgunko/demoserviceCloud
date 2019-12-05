b6d861e916e0e8b18edff51de5a38a151c58d19d
D067044 <leonid.gunko@sap.com>
2019-12-05 23:13:21 +0100

    1

--- src/components/table.component.js
+++ src/components/table.component.js
@@ -4,12 +4,37 @@ import { Table } from 'fundamental-react/Table';
 import { Panel } from 'fundamental-react/Panel';
 import { Icon } from 'fundamental-react/Icon';
 
+import { getIsAllowed } from '../httpService/service';
+
+
+function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
+
+
 
 export class MyTable extends React.Component {
 
+    componentDidMount() {
+        (async () => {
+            this.setState({ ViewAllServiceOrders: await getIsAllowed(window.sessionStorage.getItem("id_token"), "ViewAllServiceOrders", "SAP Service Cloud") })
+            this.setState({ ViewCustomerData: await getIsAllowed(window.sessionStorage.getItem("id_token"), "ViewCustomerData", "SAP Service Cloud") })
+            this.setState({ CreateServiceOrder: await getIsAllowed(window.sessionStorage.getItem("id_token"), "CreateServiceOrder", "SAP Service Cloud") })
+            this.setState({ ViewServiceOrdersAssignedToMe: await getIsAllowed(window.sessionStorage.getItem("id_token"), "ViewServiceOrdersAssignedToMe", "SAP Service Cloud") })
+        })()
+    }
+
     render() {
+        let duser = parseJwt(window.sessionStorage.getItem("id_token")) && parseJwt(window.sessionStorage.getItem("id_token"))["login_name"]
+        console.log(this.state)
         return (
-            <Panel style={{ width: "100%" }}>
+            this.state && <Panel style={{ width: "100%" }}>
                 <Panel.Header>
                     <Icon
                         glyph="order"
@@ -20,71 +45,55 @@ export class MyTable extends React.Component {
                         title="Service Orders"
                         description=""
                     />
-                    <Panel.Actions>
+                    {this.state.CreateServiceOrder && <Panel.Actions>
                         <div style={{ display: "flex" }}>
                             <Button>Add new</Button>
                         </div>
-                    </Panel.Actions>
+                    </Panel.Actions>}
                 </Panel.Header >
                 <Panel.Body style={{ background: "#edeff0" }}>
                     <Table
-                        headers={[
+                        headers={this.state.ViewCustomerData ? [
                             'Date Opened',
                             'Assigned Engineer',
                             'Customer',
                             'Customer Contact Phone',
-                        ]}
-                        tableData={
+                        ] :
                             [
-                                {
-                                    rowData: [
-                                        new Intl.DateTimeFormat('en-GB', {
-                                            year: 'numeric',
-                                            month: 'long',
-                                            day: '2-digit'
-                                        }).format(new Date(new Date().getTime() - 96 * 3600 * 1000)),
-                                        'D0111111',
-                                        'Nestle',
-                                        '+49 9876543210',
-                                    ]
-                                },
-                                {
-                                    rowData: [
-                                        new Intl.DateTimeFormat('en-GB', {
-                                            year: 'numeric',
-                                            month: 'long',
-                                            day: '2-digit'
-                                        }).format(new Date(new Date().getTime() - 72 * 3600 * 1000)),
-                                        'D067044',
-                                        'Nestle',
-                                        '+49 9876543210',
-                                    ]
-                                },
-                                {
-                                    rowData: [
-                                        new Intl.DateTimeFormat('en-GB', {
-                                            year: 'numeric',
-                                            month: 'long',
-                                            day: '2-digit'
-                                        }).format(new Date(new Date().getTime() - 48 * 3600 * 1000)),
-                                        'D067044',
-                                        'Volkswagen',
-                                        '+49 0123456789',
-                                    ]
-                                },
-                                {
-                                    rowData: [
-                                        new Intl.DateTimeFormat('en-GB', {
-                                            year: 'numeric',
-                                            month: 'long',
-                                            day: '2-digit'
-                                        }).format(new Date(new Date().getTime() - 24 * 3600 * 1000)),
-                                        'D0111111',
-                                        'Volkswagen',
-                                        '+49 0123456789',
-                                    ]
-                                },
-                            ]
+                                'Date Opened',
+                                'Assigned Engineer',
+                            ]}
+                        tableData={
+                            this.state.ViewAllServiceOrders ? data.map(row => {
+                                return {
+                                    "rowData": this.state.ViewCustomerData ? [
+                                        row.opened,
+                                        row.assignee,
+                                        row.customer,
+                                        row.phone,
+                                    ] :
+                                        [
+                                            row.opened,
+                                            row.assignee,
+                                        ]
+                                }
+                            }) :
+                                this.state.ViewServiceOrdersAssignedToMe ? data.filter(row => row.assignee === duser).map(row => {
+                                    return {
+                                        "rowData": this.state.ViewCustomerData ? [
+                                            row.opened,
+                                            row.assignee,
+                                            row.customer,
+                                            row.phone,
+                                        ] :
+                                            [
+                                                row.opened,
+                                                row.assignee,
+                                            ]
+                                    }
+                                })
+                                    :
+                                    []
                         }
                     />
                 </Panel.Body>
@@ -92,3 +101,47 @@ export class MyTable extends React.Component {
         )
     }
 }
+
+
+var data = [
+    {
+        opened: new Intl.DateTimeFormat('en-GB', {
+            year: 'numeric',
+            month: 'long',
+            day: '2-digit'
+        }).format(new Date(new Date().getTime() - 24 * 3600 * 1000)),
+        assignee: 'D0111111',
+        customer: 'Volkswagen',
+        phone: '+49 0123456789'
+    },
+    {
+        opened: new Intl.DateTimeFormat('en-GB', {
+            year: 'numeric',
+            month: 'long',
+            day: '2-digit'
+        }).format(new Date(new Date().getTime() - 48 * 3600 * 1000)),
+        assignee: 'D067044',
+        customer: 'Nestle',
+        phone: '+49 9876543210'
+    },
+    {
+        opened: new Intl.DateTimeFormat('en-GB', {
+            year: 'numeric',
+            month: 'long',
+            day: '2-digit'
+        }).format(new Date(new Date().getTime() - 72 * 3600 * 1000)),
+        assignee: 'D0111111',
+        customer: 'Nestle',
+        phone: '+49 9876543210'
+    },
+    {
+        opened: new Intl.DateTimeFormat('en-GB', {
+            year: 'numeric',
+            month: 'long',
+            day: '2-digit'
+        }).format(new Date(new Date().getTime() - 96 * 3600 * 1000)),
+        assignee: 'D067044',
+        customer: 'Volkswagen',
+        phone: '+49 0123456789'
+    },
+]
--- src/httpService/service.js
+++ src/httpService/service.js
@@ -1,61 +1,24 @@
-export const getServices = async () => {
-    let url = "https://aa4tm323i6.execute-api.eu-central-1.amazonaws.com/Prod/allServices"
-    return await (await fetch(url)).json()
-}
+export const getIsAllowed = async (token, action, service) => {
 
-export const activateOldVersion = async (service, versionId) => {
-    let url = "https://aa4tm323i6.execute-api.eu-central-1.amazonaws.com/Prod/activateOldVersion"
-    let result = await (await fetch(url, {
+    const url = `http://localhost:8080/query`;
+    const query = `{isAllowed(` +
+        `token: "` + token + `",` +
+        `action: "` + action + `",` +
+        `service: "` + service + `")}`
+    console.log(JSON.stringify({
+        query: query,
+    }))
+    return (await (await fetch(url, {
         method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
+        headers: new Headers({ 'content-type': 'application/json' }),
         body: JSON.stringify({
-            versionId: versionId,
-            service: service
+            query: query,
         }),
-    })).json()
-    console.log(result)
-    return result
+    })).json()).data.isAllowed
 }
 
-export const postNewVersion = async (serviceGroupPermissions, service) => {
-    let url = "https://aa4tm323i6.execute-api.eu-central-1.amazonaws.com/Prod/newVersion"
-    let result = await (await fetch(url, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            permissionsForGroup: serviceGroupPermissions[service],
-            service: service
-        }),
-    })).json()
-    console.log(result)
-    return result
-}
-
-export const getActiveVersions = async () => {
-    let url = "https://aa4tm323i6.execute-api.eu-central-1.amazonaws.com/Prod/activeVersions"
-    return await (await fetch(url)).json()
-}
-
-export const getAllVersions = async (service) => {
-    let url = "https://aa4tm323i6.execute-api.eu-central-1.amazonaws.com/Prod/allVersions?service=" + encodeURIComponent(service)
-    return await (await fetch(url)).json()
-}
-
-export const getGroupsForService = async (service) => {
-    let url = "https://aa4tm323i6.execute-api.eu-central-1.amazonaws.com/Prod/groupsForService?service=" + encodeURIComponent(service)
-    return await (await fetch(url)).json()
-}
-
-export const getAllGroups = async () => {
-    let url = "https://aa4tm323i6.execute-api.eu-central-1.amazonaws.com/Prod/allGroups"
-    return await (await fetch(url)).json()
-}
-
-export const getPermissionsForGroup = async (group) => {
-    let url = "https://aa4tm323i6.execute-api.eu-central-1.amazonaws.com/Prod/servicePermissionsForGroup?group=" + encodeURIComponent(group)
-    return await (await fetch(url)).json()
-}
+//{isAllowed(token: "", action: "", service: "")}
+/*
+{"query":"{isAllowed(token: \"eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJUMDAwMDAzIiwic3ViIjoiUDAwMDAwMiIsImxvZ2luX25hbWUiOiJEMDY3MDQ0IiwibWFpbCI6Imxlb25pZC5ndW5rb0BzYXAuY29tIiwiaXNzIjoiaHR0cHM6XC9cL2M0aWQtaWFtLXRlc3Qtb25lLmFjY291bnRzNDAwLm9uZGVtYW5kLmNvbSIsImdyb3VwcyI6WyJEZXZlbG9wZXJPZkNvY2twaXRUZW5hbnQxIiwiQ29ja3BpdEFkbWluaXN0cmF0b3IiLCJWaWV3ZXIiLCJTZXJ2aWNlRW5naW5lZXIiLCJTZXJ2aWNlQ29uc3VsdGFudCJdLCJsYXN0X25hbWUiOiJHdW5rbyIsImV4cCI6MTU3NTQ2MzU4NiwiaWF0IjoxNTc1NDYzMjg2LCJmaXJzdF9uYW1lIjoiTGVvbmlkIiwianRpIjoiMzU2MGYxNmEtMTRlZi00NGJlLWI4NjEtMWY3ZDljNTI0M2IzIn0.Tg0enYvR3x_xLSVnDrBeZFJJXJEZp-_41u3GfisYIhgAlf8zBf8DH2TycSU6raPfQfxgisaElnr4WzNFHEOFBrrUOtPx2NUO6zVGlO5u8luOb97c5VgBzIu16QNu6jbmvY0cxTG0kwYxBcYgyPLiEn4ztgsnCmYo_SMOF-U-eCQNtj0Hac-66hQIL2O5ZbMtu91TYbIG5YT_CdhyReVg_z0xgWhaR448AiENzBJrtuZQMIPryWZqwo1TSUa2k3E9ZTNNLSRhhMbC4JZXu3hrk_8KBEqSuW59c_2tVINdt7xakg2_uWyp1MN43Y9UrruVaKV_TQ_TwVTh3AUg7gch8Q\", action: \"ViewAllServiceOrders\", service: \"SAP Service Cloud\")}"}
+{"query":"{isAllowed(token: \"eyJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJUMDAwMDAzIiwic3ViIjoiUDAwMDAwMiIsImxvZ2luX25hbWUiOiJEMDY3MDQ0IiwibWFpbCI6Imxlb25pZC5ndW5rb0BzYXAuY29tIiwiaXNzIjoiaHR0cHM6XC9cL2M0aWQtaWFtLXRlc3Qtb25lLmFjY291bnRzNDAwLm9uZGVtYW5kLmNvbSIsImdyb3VwcyI6WyJEZXZlbG9wZXJPZkNvY2twaXRUZW5hbnQxIiwiQ29ja3BpdEFkbWluaXN0cmF0b3IiLCJWaWV3ZXIiLCJTZXJ2aWNlRW5naW5lZXIiLCJTZXJ2aWNlQ29uc3VsdGFudCJdLCJsYXN0X25hbWUiOiJHdW5rbyIsImV4cCI6MTU3NTQ2MzU4NiwiaWF0IjoxNTc1NDYzMjg2LCJmaXJzdF9uYW1lIjoiTGVvbmlkIiwianRpIjoiMzU2MGYxNmEtMTRlZi00NGJlLWI4NjEtMWY3ZDljNTI0M2IzIn0.Tg0enYvR3x_xLSVnDrBeZFJJXJEZp-_41u3GfisYIhgAlf8zBf8DH2TycSU6raPfQfxgisaElnr4WzNFHEOFBrrUOtPx2NUO6zVGlO5u8luOb97c5VgBzIu16QNu6jbmvY0cxTG0kwYxBcYgyPLiEn4ztgsnCmYo_SMOF-U-eCQNtj0Hac-66hQIL2O5ZbMtu91TYbIG5YT_CdhyReVg_z0xgWhaR448AiENzBJrtuZQMIPryWZqwo1TSUa2k3E9ZTNNLSRhhMbC4JZXu3hrk_8KBEqSuW59c_2tVINdt7xakg2_uWyp1MN43Y9UrruVaKV_TQ_TwVTh3AUg7gch8Q\", action: \"ViewAllServiceOrders\", service: \"SAP Service Cloud\")}"}
+*/
